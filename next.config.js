723da39d38be090d48d59e07bf253980c125e8de
Unknown <unknownwasd5@gmail.com>
2026-06-06 02:03:15 +0300

    a

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
 	 *
 	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 	 */
-	// basePath: "/rblx-name-color",
+	basePath: "/rblx-name-color",
 
 	/**
 	 * Disable server-based image optimization. Next.js does not support
